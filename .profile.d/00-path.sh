12dd60bdc50c750b2967f2ccfc015741ab2c8873
Joshua Barone <jbarone@justbecausesoftware.com>
2013-10-15 09:21:42 -0500

    added less

--- .profile.d/00-path.sh
+++ .profile.d/00-path.sh
@@ -1,3 +1,3 @@
 
-export PATH="/app/.heroku/python/bin:/app/.heroku/gems/bin:$PATH"
+export PATH="/app/.heroku/python/bin:/app/.heroku/gems/bin:/app/.heroku/node/bin:/app/.heroku/node/node_modules/.bin:$PATH"
 export GEM_PATH="$GEM_PATH:/app/.heroku/gems"
